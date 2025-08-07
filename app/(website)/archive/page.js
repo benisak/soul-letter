b4c76b4fd8c01444ac51e2e8f2aaefc35cd6aee2
CrisJurado13 <cristianj@dreamcode.io>
2025-08-07 11:59:03 -0500

    all content page style

--- app/(website)/archive/page.js
+++ app/(website)/archive/page.js
@@ -9,9 +9,10 @@ export default async function ArchivePage({ searchParams }) {
   return (
     <div className="bg-white py-6 px-4 md:px-[160px]">
       <div className="relative text-black">
-        <h1 className="font-nunito-sans text-center text-3xl font-semibold tracking-tight dark:text-white lg:text-4xl lg:leading-snug">
+        <h1 className="font-nunito-sans text-center text-4xl font-bold leading-normal text-[var(--Black-500,#1F1F1F)] dark:text-white">
           All content
         </h1>
+
         {searchParams ? (
           <Archive searchParams={searchParams} />
         ) : (
